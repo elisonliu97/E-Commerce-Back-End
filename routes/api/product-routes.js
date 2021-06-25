02f11cbddb2c381d048f9c42076c10c2c38b2387
Elison Liu <elisonliu97@gmail.com>
2021-06-24 23:01:25 -0400

    routing for deleting specific product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,8 +109,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id : req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'ID NOT FOUND' });
+    } else {
+      res.status(200).json({ message: 'PRODUCT DELETED' });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
