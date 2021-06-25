be62a13b524adeba6ee49718c5ce499d70ef52f4
Elison Liu <elisonliu97@gmail.com>
2021-06-24 22:51:20 -0400

    routing to get all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }]
+    });
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
