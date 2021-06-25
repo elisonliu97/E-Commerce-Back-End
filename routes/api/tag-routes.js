3d5a89d8cc81130d5a4b00780891e2fc06012eb4
Elison Liu <elisonliu97@gmail.com>
2021-06-24 23:08:41 -0400

    routing to create tag data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,8 +34,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
