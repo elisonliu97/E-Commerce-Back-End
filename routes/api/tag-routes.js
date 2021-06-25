dc9eeed1e3e7c5c48239b59535b2934741b64d7d
Elison Liu <elisonliu97@gmail.com>
2021-06-24 23:07:19 -0400

    routing to get data of specific tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const e = require('express');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
@@ -16,9 +17,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'ID NOT FOUND' });
+    } else {
+      res.status(200).json(tagData);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
