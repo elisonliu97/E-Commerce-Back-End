3f09bbbae6b965201ec54cb610a7d04be317b315
Elison Liu <elisonliu97@gmail.com>
2021-06-24 22:54:26 -0400

    routing to get specific product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,9 +18,21 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'ID NOT FOUND' });
+    } else {
+      res.status(200).json(productData);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
